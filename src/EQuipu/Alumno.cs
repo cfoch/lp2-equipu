99f0070f7cd8a6f6332c41da8077888c66f26781
Fabian Orccon <fabian.orccon@pucp.pe>
2017-04-01 07:21:52 +0200

    src: Agregar clase Alumno

---
+++ src/EQuipu/Alumno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using EQuipu.utils;
+
+namespace EQuipu
+{
+    public class Alumno : Miembro
+    {
+        private int codigoAlumno;
+        private double _CRAEST;
+
+        public Alumno(int codigo, int codigoAlumno, string nombre, DateTime fechaNacimiento, string direccion,
+            string email, Sexo sexo, double CRAEST)
+            : base(codigo, nombre, fechaNacimiento, direccion, email, sexo)
+        {
+            this.CodigoAlumno = CodigoAlumno;
+            this._CRAEST = CRAEST;
+        }
+
+        public int CodigoAlumno { get => codigoAlumno; set => codigoAlumno = value; }
+        public double CRAEST { get => _CRAEST; set => _CRAEST = value; }
+    }
+}
