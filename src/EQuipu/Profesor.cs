3486ab1e50d569d4139110e24559b3469450db90
Fabian Orccon <fabian.orccon@pucp.pe>
2017-04-01 07:22:28 +0200

    src: Agregar clase Profesor

---
+++ src/EQuipu/Profesor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using EQuipu.utils;
+
+namespace EQuipu
+{
+    public class Profesor : Miembro
+    {
+        private int codigoProfesor;
+        private string estado;
+
+        public Profesor(int codigo, int codigoProfesor, string nombre, DateTime fechaNacimiento, string direccion,
+            string email, Sexo sexo, string estado = null)
+            : base(codigo, nombre, fechaNacimiento, direccion, email, sexo)
+        {
+            this.CodigoProfesor = codigoProfesor;
+            this.Estado = estado;
+        }
+
+        public int CodigoProfesor { get => codigoProfesor; set => codigoProfesor = value; }
+        public string Estado { get => estado; set => estado = value; }
+    }
+}
