e13cb6903fb826a03cc74235358e45ced3ccf0fb
Fabian Orccon <fabian.orccon@pucp.pe>
2017-04-01 07:22:10 +0200

    src: Agregar clase Externo

---
+++ src/EQuipu/Externo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using EQuipu.utils;
+
+namespace EQuipu
+{
+    public class Externo : Miembro
+    {
+        private Dedicacion dedicacion;
+        private string tipo;
+   
+        public Externo(int codigo, string nombre, DateTime fechaNacimiento, string direccion, string email, Sexo sexo,
+            Dedicacion dedicacion, string tipo)
+            : base(codigo, nombre, fechaNacimiento, direccion, email, sexo)
+        {
+            this.Dedicacion = dedicacion;
+            this.Tipo = tipo;
+        }
+
+        public Dedicacion Dedicacion { get => dedicacion; set => dedicacion = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+    }
+}
